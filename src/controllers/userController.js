2b103f7fd1094e28b85ca15c31710b2a7be15bc8
Jecheon Ryu <rjc1704@gmail.com>
2025-11-13 18:57:35 +0900

    일부수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,7 +22,7 @@ userController.post("/users", async (req, res, next) => {
 userController.post("/login", async (req, res, next) => {
   try {
     // 1. 요청 바디에서 로그인시 필요한 email, password 추출
-    // 2. 각 속성이 없으면 422 코드와 "email, password 가 모두 필요합니다." 에러메시지로 에러 발생
+    // 2. 각 속성이 없으면 400 코드와 "email, password 가 모두 필요합니다." 에러메시지로 에러 발생
     // 3. userService 에서 getUser 호출
     // 4. 로그인 성공 시 200 코드로 응답와 함께 로그인한 유저 데이터 반환
     // 5. 로그인 실패 시 에러핸들러로 에러 전달 (이미 구현되어 있음)
