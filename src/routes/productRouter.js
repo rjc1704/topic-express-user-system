8371b96648ebf51b00418272f4989c1da5228eb1
Jecheon Ryu <rjc1704@gmail.com>
2025-11-17 23:20:41 +0900

    명칭 변경

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,17 +2,17 @@ import express from "express";
 import auth from "../middlewares/auth.js";
 import productService from "../services/productService.js";
 
-const productController = express.Router();
+const productRouter = express.Router();
 
-productController.post("/", auth.verifyAccessToken, async (req, res, next) => {
+productRouter.post("/", auth.verifyAccessToken, async (req, res, next) => {
   const createdProduct = await productService.create(req.body);
   return res.json(createdProduct);
 });
 
-productController.get("/:id", async (req, res) => {
+productRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
   const product = await productService.getById(id);
   return res.json(product);
 });
 
-export default productController;
+export default productRouter;
